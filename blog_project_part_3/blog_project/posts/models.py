10b51df4cf718cb5ebc534c0fb59933c9dcf1d16
unknown <your_email@example.com>
2025-12-05 21:22:26 +0600

    details post update

--- blog_project_part_3/blog_project/posts/models.py
+++ blog_project_part_3/blog_project/posts/models.py
@@ -9,12 +9,19 @@ class Post(models.Model):
     content = models.TextField()
     category = models.ManyToManyField(Category) #Ekta post multiple categorir moddhe thakte pare abr ekta categorir moddhe multiple post thakte pare
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts/media/uploads/', blank = True, null = True)
-    
-
+    image = models.ImageField(upload_to='uploads/', blank = True, null = True)
 
     def __str__(self):
         return self.title
-    
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comments by {self.name}"
     
